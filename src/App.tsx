3ad76ab933acae6fbdc53c3b2358adb2d53fc960
AndreiJoroveanu <joroveanuandrei@gmail.com>
2025-03-15 15:04:16 +0200

    Moved the Suspense and ErrorBoundary from wrapping each route component to the React Router Outlet itself, and removed a Pokémon Details page wrapper, since it is no longer necessary. These are to prepare for a project restructuring and reorganizing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
-import { ElementType, lazy, Suspense } from "react";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-  useParams,
-} from "react-router";
-import { ErrorBoundary } from "react-error-boundary";
+import { lazy } from "react";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 
-import ErrorMessage from "@/ui/ErrorMessage.tsx";
-import Loader from "@/ui/Loader.tsx";
 import AppLayout from "@/ui/AppLayout.tsx";
 
 const AllPokemonPage = lazy(() => import("@/pages/AllPokemon.tsx"));
 const PokemonDetailsPage = lazy(() => import("@/pages/PokemonDetails.tsx"));
 const AllMovesPage = lazy(() => import("@/pages/AllMoves.tsx"));
 const MoveDetailsPage = lazy(() => import("@/pages/MoveDetails.tsx"));
-
-// This is needed to reset the key when the user clicks on
-// a different Pokémon link from the Pokémon details page
-const PokemonDetailsPageWrapper = () => {
-  const { id } = useParams();
-  return <PokemonDetailsPage key={id} />;
-};
-
-// Wrapper for Suspense and ErrorBoundary
-const wrapper = (Component: ElementType) => (
-  <ErrorBoundary
-    FallbackComponent={({ error }: { error: Error }) => (
-      <ErrorMessage errors={[error.message]} />
-    )}
-  >
-    <Suspense
-      fallback={
-        <div className="h-screen bg-slate-100 transition-colors dark:bg-slate-900">
-          <Loader size={24} displaysText={true} />
-        </div>
-      }
-    >
-      <Component />
-    </Suspense>
-  </ErrorBoundary>
-);
 
 const router = createBrowserRouter([
   {
@@ -53,19 +19,19 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/pokedex/pokemon",
-        element: wrapper(AllPokemonPage),
+        element: <AllPokemonPage />,
       },
       {
         path: "/pokedex/pokemon/:id",
-        element: wrapper(PokemonDetailsPageWrapper),
+        element: <PokemonDetailsPage />,
       },
       {
         path: "/pokedex/moves",
-        element: wrapper(AllMovesPage),
+        element: <AllMovesPage />,
       },
       {
         path: "/pokedex/moves/:id",
-        element: wrapper(MoveDetailsPage),
+        element: <MoveDetailsPage />,
       },
     ],
   },
--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,6 +1,10 @@
+import { Suspense } from "react";
 import { Outlet, ScrollRestoration, useLocation } from "react-router";
+import { ErrorBoundary } from "react-error-boundary";
 
 import Navbar from "@/ui/Navbar.tsx";
+import Loader from "@/ui/Loader.tsx";
+import ErrorMessage from "@/ui/ErrorMessage.tsx";
 
 const AppLayout = () => {
   const location = useLocation();
@@ -15,7 +19,22 @@ const AppLayout = () => {
 
       <Navbar />
 
-      <Outlet key={location.pathname} />
+      <Suspense
+        fallback={
+          <div className="h-screen bg-slate-100 transition-colors dark:bg-slate-900">
+            <Loader size={24} displaysText={true} />
+          </div>
+        }
+        key={location.key}
+      >
+        <ErrorBoundary
+          FallbackComponent={({ error }: { error: Error }) => (
+            <ErrorMessage errors={[error.message]} />
+          )}
+        >
+          <Outlet />
+        </ErrorBoundary>
+      </Suspense>
 
       <ScrollRestoration />
     </div>
