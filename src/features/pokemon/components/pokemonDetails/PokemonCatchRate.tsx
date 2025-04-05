1bf9954fff76bcfb4136ced73128e35cb4a38a1a
AndreiJoroveanu <joroveanuandrei@gmail.com>
2025-04-05 07:31:21 +0300

    Fixed the hover pop-up from the Pokémon catch probability display to no longer block pointer events to elements behind it when invisible.

--- src/features/pokemon/components/pokemonDetails/PokemonCatchRate.tsx
+++ src/features/pokemon/components/pokemonDetails/PokemonCatchRate.tsx
@@ -11,21 +11,28 @@ const PokemonCatchRate = memo(({ catchRate }: CatchRateProps) => {
 
   return (
     <p className="my-2">
-      Catch Rate: {catchRate ?? "Loading..."}
       {catchRate ? (
-        <span className="relative">
-          {" ("}
-          <span className="peer border-b-3 border-dotted">{probability}%</span>
+        <>
+          <span className="font-bold text-slate-700 transition-[color] dark:text-slate-300">
+            Catch Rate:
+          </span>
+
+          {` ${catchRate} (`}
+          <span className="group relative border-b-3 border-dotted">
+            {probability}%
+            <div className="pointer-events-none absolute bottom-full left-1/2 mb-1 -translate-x-1/2 rounded bg-slate-800 px-3 py-1 text-center text-sm text-nowrap text-slate-200 opacity-0 shadow-md transition-opacity group-[&:hover]:opacity-100 dark:bg-slate-200 dark:text-slate-800 dark:shadow-none">
+              {probability}% chance to catch at
+              <br />
+              full HP with an ordinary
+              <br />
+              Poké Ball in Generation VI+
+            </div>
+          </span>
           {")"}
-          <div className="absolute bottom-full left-1/2 mb-1 -translate-x-1/2 rounded bg-slate-800 px-3 py-1 text-center text-sm text-nowrap text-slate-200 opacity-0 shadow-md transition-opacity peer-[&:hover]:opacity-100 dark:bg-slate-200 dark:text-slate-800 dark:shadow-none">
-            {probability}% chance to catch at
-            <br />
-            full HP with an ordinary
-            <br />
-            Poké Ball in Generation VI+
-          </div>
-        </span>
-      ) : null}
+        </>
+      ) : (
+        "Loading..."
+      )}
     </p>
   );
 });
