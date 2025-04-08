3f715f93fcc59e47797bcae4c9ce75810b9d0971
AndreiJoroveanu <joroveanuandrei@gmail.com>
2025-04-08 07:37:36 +0300

    Changed the background color before the React app loads, to use the system color Canvas.

--- src/features/pokemon/components/pokemonDetails/PokemonCatchRate.tsx
+++ src/features/pokemon/components/pokemonDetails/PokemonCatchRate.tsx
@@ -20,13 +20,13 @@ const PokemonCatchRate = memo(({ catchRate }: CatchRateProps) => {
           {` ${catchRate} (`}
           <span className="group relative border-b-3 border-dotted">
             {probability}%
-            <div className="pointer-events-none absolute bottom-full left-1/2 mb-1 -translate-x-1/2 rounded bg-slate-800 px-3 py-1 text-center text-sm text-nowrap text-slate-200 opacity-0 shadow-md transition-opacity group-[&:hover]:opacity-100 dark:bg-slate-200 dark:text-slate-800 dark:shadow-none">
+            <span className="pointer-events-none absolute bottom-full left-1/2 mb-1 -translate-x-1/2 rounded bg-slate-800 px-3 py-1 text-center text-sm text-nowrap text-slate-200 opacity-0 shadow-md transition-opacity group-[&:hover]:opacity-100 dark:bg-slate-200 dark:text-slate-800 dark:shadow-none">
               {probability}% chance to catch at
               <br />
               full HP with an ordinary
               <br />
               Poké Ball in Generation VI+
-            </div>
+            </span>
           </span>
           {")"}
         </>
