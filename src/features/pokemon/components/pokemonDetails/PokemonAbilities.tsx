9ee9d4fec5aeb8ff66960240f6359dfd29f96212
AndreiJoroveanu <joroveanuandrei@gmail.com>
2025-05-22 19:40:34 +0300

    Fixed Pokémon having potentially duplicate ability entries.

--- src/features/pokemon/components/pokemonDetails/PokemonAbilities.tsx
+++ src/features/pokemon/components/pokemonDetails/PokemonAbilities.tsx
@@ -1,4 +1,5 @@
 import type { PokemonAbility } from "pokedex-promise-v2";
+
 import PokemonAbilityDisplay from "@/features/pokemon/components/pokemonDetails/PokemonAbilityDisplay.tsx";
 import Loader from "@/components/Loader.tsx";
 
@@ -14,9 +15,24 @@ const PokemonAbilities = ({ abilities }: AbilitiesProps) => (
 
     <div className="mb-4 flex max-w-lg flex-col gap-4 rounded-xl bg-slate-200 px-4 py-2 shadow-lg transition-[background-color] dark:bg-slate-800 dark:shadow-none">
       {abilities ? (
-        abilities.map((ability) => (
-          <PokemonAbilityDisplay key={ability.ability.name} ability={ability} />
-        ))
+        // Deduplicates abilities by ability name
+        Array.from(
+          new Map(
+            abilities
+              // Reverse to preserve only the first occurrence in case of duplicates
+              .slice()
+              .reverse()
+              .map((a) => [a.ability.name, a]),
+          ).values(),
+        )
+          // Reverse back to preserve the original ability order
+          .reverse()
+          .map((ability) => (
+            <PokemonAbilityDisplay
+              key={ability.ability.name}
+              ability={ability}
+            />
+          ))
       ) : (
         <div className="h-24">
           <Loader size={16} />
